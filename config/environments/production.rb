ba085ca9305dc9b863d2602820d1c5a38ac07686
Ryan <ryananthonyrichardson@gmail.com>
2023-07-21 00:11:13 -0700

    feat: rename app to draftordie

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fantasy_draft_production"
+  # config.active_job.queue_name_prefix = "draftordie_production"
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
