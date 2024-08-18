b4a251ab5613ac19dd372c248c14747e2f0c1a18
Ryan <ryananthonyrichardson@gmail.com>
2024-08-18 11:36:49 -0700

    feat: add fly health check

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -31,6 +31,11 @@ class DraftsController < ApplicationController
     redirect_to root_path
   end
 
+  def progress
+    ProgressDraftsJob.perform_now
+    head :ok
+  end
+
   def verify_access
     access_code =
       params[:access_code].upcase
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
 
+  get 'bnzg3si3tbs3yb5ya48o', to: "drafts#progress"
+
   if Rails.env.development?
     get :docs, to: "pages#docs"
   end
